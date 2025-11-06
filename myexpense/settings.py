229d4a215b82b67fe6d9b77b12640e05fa5fd09f
thriveni <sthriveni58@gmail.com>
2025-11-06 23:27:17 +0530

    Add requirements.txt

--- myexpense/settings.py
+++ myexpense/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@c(1em&mt-7t1mwf$qc=_gk7uix3x#miw(a&xzq1)4-lnr-@kk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
